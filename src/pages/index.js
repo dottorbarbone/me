5fc26e434f7445b4cec8cf286071ccf41048edd1
dottorbarbone <eli4b4rb4ric@gmail.com>
2026-05-17 18:52:23 +0200

    build

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Typography } from "@mui/material";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
